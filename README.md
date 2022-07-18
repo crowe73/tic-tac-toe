54c0b7f5847a8afafe7a130c79bfa5dc1d43756d
crowe73 <rowechad73@yahoo.com>
2022-07-17 20:19:40 -0500

    Gameboard and renderArrayToScreenModule module
    Add gameboard elements to index.html.
    Add renderArrayToScreenModule.
    Add css style rules to display gameboard.

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 07/11/2022 21:35 - Edit title element in index.html.
 
 07/11/2022 22:34 - Set up modules and factory functions.
+
+07/13/2022 23:00 - Gameboard and renderArrayToScreenModule module.
--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 // Setup the gameboard module.
 let gameBoardModule = (() => 
 {
-    let gameBoard = [];
-    return {};
+    let gameBoard = ["x"];
+    return {gameBoard};
 })();
 
 // Setup the displayController module.
@@ -26,8 +26,20 @@ let createPlayer = (playerName, playerNumber, assignedXO) =>
     return {getPlayerName, playerName, playerNumber, assignedXO};
 };
 
+// Set up the render module for array display to the gameboard.
+let renderArrayToScreenModule = (() => 
+{
+    const gridBoxes = document.querySelectorAll(".grid-box");
+    gridBoxes[0].textContent = gameBoardModule.gameBoard;
+    console.log("Show me the array as seen inside of renderArrayToScreenModule...", gameBoardModule.gameBoard);
+    console.log("Show me the node list of the gridBoxes as seeninside of renderArrayToScreenModule...", gridBoxes);
+    return {};
+})();
+
 // TODO: Create inputs for name, and assignedXO - asking them for their preference.
 // TODO: Do I need to assign player numbers?
 
 let Chad = createPlayer("Chad", 1, "x");
 let Kandy = createPlayer("Kandy", 2, "o");
+
+renderArrayToScreenModule;
